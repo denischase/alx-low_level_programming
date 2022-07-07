da729a0f7ddf4ab20be8e0f72583c5709e9c5914
denischase <chasedenis@gmail.com>
2022-07-07 15:21:37 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - A function that checks the product of two numbers
+ * @a: An integer
+ * @b: An integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
