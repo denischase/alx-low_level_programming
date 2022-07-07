470e89667598e17dc9914e3ccd008cc4a15c9c4c
denischase <chasedenis@gmail.com>
2022-07-06 19:23:25 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - Prints every minute of the day of
- * 	Jack Bauer, starting from 00:00 to 23:59.
+ *	Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -16,6 +16,7 @@ void jack_bauer(void)
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
+			_putchar((M % 10) + '0');
 			_putchar('\n');
 		}
 	}
